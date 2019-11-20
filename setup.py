60f9d5d4ce55ffd3f9a062af0b8ba47e63f4c5ce
Bobbycookie <35229441+Bobbycookie@users.noreply.github.com>
2019-11-19 19:36:53 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="package_lucas",
+    name="llomuscio_package",
     version="0.0.1",
     author="Lucas L",
     author_email="lucas@example.com",
